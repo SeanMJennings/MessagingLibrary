ef37f8e953b1fb8f4c59353c623aba47bf305b49
SeanJennings <seanjennings976@gmail.com>
2026-01-01 11:18:24 +0000

    Revert "remove service bus stuff as will be using mass transit now. keep storage queue stuff which mass transit does not support?"
    This reverts commit 94bd4674397ca45c35d1182484a02aaa015bbe8b.

---
+++ Messengers/ServiceBus/ServiceBusEventPublisher.cs
@@ -0,0 +1,55 @@
+﻿using Azure.Messaging.ServiceBus;
+using Messages;
+
+namespace Messengers.ServiceBus;
+
+public interface IAmAServiceBusEventPublisher
+{
+    Task PublishToTopic<T>(T theEvent) where T : Event;
+}
+
+public sealed class ServiceBusEventPublisher : IAmAServiceBusEventPublisher
+{
+    private readonly ServiceBusSender sender;
+    
+    internal ServiceBusEventPublisher(string topicName, IAmAServiceBus serviceBus)
+    {
+        sender = serviceBus.CreateSender(topicName);
+    }
+    
+    public static ServiceBusEventPublisher New(string connectionString, string topicName)
+    {
+        return new ServiceBusEventPublisher(topicName, ServiceBus.New(connectionString));
+    }
+    
+    public Task PublishToTopic<T>(T theEvent) where T : Event
+    { 
+        return sender.SendMessageAsync(new ServiceBusMessage(JsonSerialization.Serialize(theEvent)));
+    }
+}
+
+public sealed class ServiceBusEventPublisherFactory
+{
+    private readonly IAmAServiceBus serviceBus;
+    
+    internal ServiceBusEventPublisherFactory(IAmAServiceBus serviceBus)
+    {
+        this.serviceBus = serviceBus;
+    }
+
+    public ServiceBusEventPublisherFactory(string connectionString)
+    {
+        serviceBus = ServiceBus.New(connectionString);
+    }
+
+    public async Task<ServiceBusEventPublisher> CreateServiceBusEventPublisherEnsuringTopicExists(string topicName)
+    {
+        await serviceBus.CreateTopicIfNotExistsAsync(topicName);
+        return new ServiceBusEventPublisher(topicName, serviceBus);
+    }    
+    
+    public ServiceBusEventPublisher CreateServiceBusEventPublisher(string topicName)
+    {
+        return new ServiceBusEventPublisher(topicName, serviceBus);
+    }
+}
